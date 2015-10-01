d45989aca2cf8b7864c2606ead7a2fb7095f4890
jeffchao98 <philadelphiafs@gmail.com>
2015-10-02 00:22:51 +0900

    Create
    Repository create

---
+++ Solution/__init__.py
@@ -0,0 +1,14 @@
+
+
+str_a = 'testset'
+str_b = 'kerker'
+i_test = 1236321
+str_a = str(i_test)
+
+print(str_a == str_b)
+
+i_lenStrA = len(str_a)
+str_a_rev = str_a[::-1]
+
+print(str_a_rev == str_a)
+
