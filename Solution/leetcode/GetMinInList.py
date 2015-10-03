d8968f768ab5e0debe815748d910ad905e4ce562
jeffchao98 <philadelphiafs@gmail.com>
2015-10-03 21:42:12 +0900

    [Submit]Find Minimum in Rotated Sorted Array
    LeetCode Id : 153
    Result : Accepted
    Run Time : 48 ms

---
+++ Solution/leetcode/GetMinInList.py
@@ -0,0 +1,14 @@
+class GetMinInList(object):
+    def findMin(self, nums):
+        """
+        Problem State : Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+        (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2)
+        type nums: List[int]
+        return type: int
+        Idea : Sort -> Get the first number + length check
+        """
+        nums.sort()
+        if len(nums)>0:
+            return nums[0]
+        else:
+            return 0
